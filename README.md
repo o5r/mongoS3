034b3d67944eab218525d0f3451fa63e6e5681f6
Julien Guepin <guepin@lendix.com>
2015-11-25 19:50:01 +0100

    Camelcase opts variables

--- README.md
+++ README.md
@@ -7,12 +7,12 @@ Simply dump a mongodb, then stream everything to AWS S3, promise flavoured
 const mongodump = require('mongodump');
 
 mongodump({
-  URI: process.env.MONGO_URI,
-  S3: {
+  uri: process.env.MONGO_URI,
+  s3: {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      Bucket: process.env.AWS_S3_DUMP_BUCKET,
-      Key: process.env.AWS_S3_DUMP_KEY
+      bucket: process.env.AWS_S3_DUMP_BUCKET,
+      key: process.env.AWS_S3_DUMP_KEY
   }
 }).then(function (result) { console.log('EVERYTHING SHOULD BE OK') })
   .catch(function (err) { console.error('Snap something went wrong :(') });
--- test/index.js
+++ test/index.js
@@ -4,12 +4,12 @@ describe('mongudump test suite', function () {
     this.timeout(10000);
     it('work', function (done) {
         return mongodump({
-            URI: process.env.MONGO_URI,
-            S3: {
+            uri: process.env.MONGO_URI,
+            s3: {
                 accessKeyId: process.env.AWS_ACCESS_KEY_ID,
                 secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-                Bucket: process.env.AWS_S3_DUMP_BUCKET,
-                Key: process.env.AWS_S3_DUMP_KEY
+                bucket: process.env.AWS_S3_DUMP_BUCKET,
+                key: process.env.AWS_S3_DUMP_KEY
             }
         }).then(function () {
             done();
