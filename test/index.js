8ede226dbee93102f3a6d4f90de20385500f8b9f
Jordan Van Walleghem <j.vanwall@gmail.com>
2015-12-01 15:37:37 +0100

    Now with compression !

--- test/index.js
+++ test/index.js
@@ -1,18 +1,21 @@
 var mongodump = require('../index');
 
-describe('mongudump test suite', function () {
-    this.timeout(100000);
-    it('work', function (done) {
-        return mongodump({
-            uri: process.env.MONGO_URI,
-            s3: {
-                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-                bucket: process.env.AWS_S3_DUMP_BUCKET,
-                key: process.env.AWS_S3_DUMP_KEY
-            }
-        }).then(function () {
-            done();
-        }).catch(done);
+describe('mongudump test suite', function() {
+  this.timeout(100000);
+  it('work', function(done) {
+    return mongodump({
+      uri: process.env.MONGO_URI,
+      s3: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        bucket: process.env.AWS_S3_DUMP_BUCKET,
+        key: process.env.AWS_S3_DUMP_KEY
+      }
+    }).then(function() {
+      done();
+    }).catch(function(err) {
+      console.error(err.trace);
+      done(err);
     });
+  });
 });
